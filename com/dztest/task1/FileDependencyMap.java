db772b0e09be244014909147f07ea55b9b9de988
aslanLinuxLaptop <nurlybekov.aslan@yandex.kz>
2024-10-31 22:21:11 +0300

    Class of FileGraph as Map data structure

---
+++ com/dztest/task1/FileDependencyMap.java
@@ -0,0 +1,35 @@
+package com.dztest.task1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FileDependencyMap {
+    private Map<String, Set<String>> graph = new HashMap<>();
+
+    public void addDependency(String file, String dependency) {
+
+        //check is such file exist in real
+
+        graph.computeIfAbsent(file, k -> new HashSet<>()).add(dependency);
+
+        graph.putIfAbsent(dependency, new HashSet<>());
+    }
+
+    public void addIndependentFile(String file) {
+        // Add the file with no dependencies
+        // if graph is disconnected It will be presented in alphabetic order
+        graph.putIfAbsent(file, new HashSet<>());
+    }
+
+    public Set<String> getDependencies(String file) {
+        return graph.getOrDefault(file, new HashSet<>());
+    }
+
+    public void printGraph() {
+        for (String file : graph.keySet()) {
+            System.out.println(file + " -> " + graph.get(file));
+        }
+    }
+}
--- com/dztest/task1/Main.java
+++ com/dztest/task1/Main.java
@@ -1,23 +1,35 @@
 package com.dztest.task1;
 
 
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Path rootPath = Paths.get("resources");
+      //  Path rootPath = Paths.get("resources");
 
-        List<File> allFiles = new ArrayList<>();
-        listAllFiles(rootPath, allFiles);
+       // List<File> allFiles = new ArrayList<>();
 
+        // listAllFiles(rootPath, allFiles);
+        FileDependencyMap fileDependencyMap = new FileDependencyMap();
+
+
+        fileDependencyMap.addDependency("Folder 2/File 2-2", "Folder 1/File 1-1");
+        fileDependencyMap.addDependency("Folder 2/File 2-2", "Folder 2/File 2-1");
+
+        fileDependencyMap.addDependency("Folder 1/File 1-1", "Folder 2/File 2-1");
+
+        fileDependencyMap.addIndependentFile("Folder 0/File 0-0");
+
+        fileDependencyMap.printGraph();
 
     }
 

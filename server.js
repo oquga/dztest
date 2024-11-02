b15d8815ebb65da4b78714f92fbbeab6ca06b5b2
Aslan Nurlybekov <nurlybekov.aslan@yandex.kz>
2024-11-02 20:47:01 +0300

    request handler fix server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const https = require("https");
 const url = require("url");
-const fs = require('fs');
 
 const PORT = 3000;
 
@@ -33,27 +32,7 @@ const server = http.createServer((req, res) => {
   // Разбираем URL запроса
   const parsedUrl = url.parse(req.url, true);
 
-  if (req.method === "GET" && parsedUrl.pathname === "/") {
-    // Read the index.html file
-    fs.readFile('public/index.html', 'utf8', (err, data) => {
-      if (err) {
-        // If there's an error reading the file, respond with a 500 status
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-        return;
-      }
-      // If successful, respond with the content of index.html
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
-  } else {
-    // Handle 404 for other routes
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('Not Found');
-  }
-
-
-    if (req.method === "GET" && parsedUrl.pathname === "/todos") {
+  if (req.method === "GET" && parsedUrl.pathname === "/todos") {
     // Получаем параметры из строки запроса
     const limit = parsedUrl.query.limit || ""; // Значение по умолчанию 2
     const offset = parsedUrl.query.offset || ""; // Значение по умолчанию 0
